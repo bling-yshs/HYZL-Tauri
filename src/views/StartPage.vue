a064d94a9ec6beab2d43e9cb35876357105ccc3c
bling-yshs <377178599@qq.com>
2023-10-21 15:03:49 +0800

    test: 修改更新文件，使其指向 Github Release (昨天弄了一下午，结果人家 action 早就自带了，像个小丑)

--- src/views/StartPage.vue
+++ src/views/StartPage.vue
@@ -3,6 +3,7 @@
     <a-space direction="vertical">
       <a-space direction="horizontal">
         <a-button type="primary" @click="startWithApi">启动云崽&签名API</a-button>
+        <a-button type="default" @click="downloadTest">下载测试</a-button>
       </a-space>
     </a-space>
   </NormalContent>
@@ -12,19 +13,35 @@ import NormalContent from "./NormalContent.vue";
 import {message} from "ant-design-vue";
 import {invoke} from "@tauri-apps/api/tauri";
 import {DataResponse} from "../entity/response.ts";
+import {getClient, ResponseType} from "@tauri-apps/api/http";
+import * as fs from "@tauri-apps/api/fs";
+
+//下载测试
+const downloadTest = async () => {
+  const url = 'https://gitee.com/bling_yshs/YzLauncher-windows/releases/download/v0.1.58/yzMD5.txt'
+  const client = await getClient();
+  let response = await client.get(url, {
+    responseType: ResponseType.Binary
+  });
+  await fs.writeBinaryFile(
+    'a.txt',
+    response.data as fs.BinaryFileContents
+  )
+  message.success("yes")
+}
+
+
 // 启动云崽&签名API
 const startWithApi = async () => {
   const response = await invoke('start_yunzai_and_api');
   const res = JSON.parse(response as string) as DataResponse
   if (res.code === 200) {
     message.success(res.message)
-    message.success('启动成功')
     return
   } else {
     message.error('启动失败')
     return
   }
-  
 }
 </script>
 <style scoped>
