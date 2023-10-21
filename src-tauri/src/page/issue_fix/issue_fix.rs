710fa36f2b9edd57ccce5d4d15ed4a2d19479df6
bling-yshs <377178599@qq.com>
2023-10-21 14:30:14 +0800

    feat(问题修复): 重装依赖

---
+++ src-tauri/src/page/issue_fix/issue_fix.rs
@@ -0,0 +1,21 @@
+use crate::entity::hyzl_path;
+use crate::entity::return_data::ReturnData;
+use crate::utils::cmd_command::{run_command, WindowType};
+
+#[tauri::command]
+pub async fn reinstall_dependence() -> String {
+    //检查YUNZAI_DIR文件夹下是否存在node_modules文件夹，如有则删除他
+    if hyzl_path::YUNZAI_DIR.join("node_modules").exists() {
+        match std::fs::remove_dir_all(hyzl_path::YUNZAI_DIR.join("node_modules")) {
+            Ok(_) => {}
+            Err(_) => return ReturnData::fast_failure("删除node_modules文件夹失败"),
+        };
+    }
+    //在YUNZAI_DIR文件夹下执行pnpm install
+    let output = run_command(&hyzl_path::YUNZAI_DIR, "pnpm install", WindowType::Default);
+    if output.status.success() {
+        return ReturnData::fast_success("重装依赖成功");
+    } else {
+        return ReturnData::fast_failure("重装依赖失败");
+    }
+}
--- src-tauri/src/page/start/start.rs
+++ src-tauri/src/page/start/start.rs
@@ -1,5 +1,4 @@
 use std::fs;
-use std::process::Command;
 use std::str::from_utf8;
 
 use serde_json::json;
@@ -7,8 +6,7 @@ use log::{info, warn};
 
 use return_data::ReturnData;
 
-use crate::entity::path;
-use crate::entity::path::{SIGN_API_DIR, YUNZAI_DIR};
+use crate::entity::hyzl_path;
 use crate::entity::return_data;
 use crate::utils::cmd_command::{run_command, WindowType};
 
@@ -19,15 +17,15 @@ pub async fn start_yunzai() -> String {
 
 #[tauri::command]
 pub async fn start_yunzai_and_api() -> String {
-    if !SIGN_API_DIR.exists() {
+    if !hyzl_path::SIGN_API_DIR.exists() {
         return ReturnData::fast_failure("签名API文件夹不存在");
     }
     tokio::spawn(async {
-        run_command(&path::SIGN_API_DIR,"start.bat",WindowType::WithWindowKeepOpen);
+        run_command(&hyzl_path::SIGN_API_DIR, "start.bat", WindowType::WithWindowKeepOpen);
     });
 
     tokio::spawn(async {
-        run_command(&path::YUNZAI_DIR,"node app",WindowType::WithWindowKeepOpen);
+        run_command(&hyzl_path::YUNZAI_DIR, "node app", WindowType::WithWindowKeepOpen);
     });
 
     return ReturnData::fast_success("启动成功");
@@ -36,7 +34,7 @@ pub async fn start_yunzai_and_api() -> String {
 #[tauri::command]
 pub async fn download_sign_api() -> String {
     info!("调用了 download_sign_api fn");
-    let output = run_command(&path::PROGRAM_DIR, "git clone --depth 1 https://github.com/bling-yshs/unidbg-fetch-qsign", WindowType::Default);
+    let output = run_command(&hyzl_path::PROGRAM_DIR, "git clone --depth 1 https://github.com/bling-yshs/unidbg-fetch-qsign", WindowType::Default);
     if output.status.success() {
         info!("API下载成功");
         match fs::rename("unidbg-fetch-qsign", "API") {
@@ -54,6 +52,6 @@ pub async fn download_sign_api() -> String {
 
 #[test]
 fn t() {
-    let output = run_command(&path::PROGRAM_DIR, "chdidd", WindowType::Default);
+    let output = run_command(&hyzl_path::PROGRAM_DIR, "chdidd", WindowType::Default);
     if output.stderr.is_empty() { println!("空的！") } else { println!("错误") }
 }
