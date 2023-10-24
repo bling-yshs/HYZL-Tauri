0ffa6a66c277982d996534964890c0cca21190cf
bling-yshs <377178599@qq.com>
2023-10-24 13:42:51 +0800

    feat: 添加文件夹初始化

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
-import './assets/main.css'
-import router from './router'
-// ant-design
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/reset.css';
 
 const app = createApp(App);
+
 // ant-design
+import 'ant-design-vue/dist/reset.css';
+import Antd from 'ant-design-vue';
+
 app.use(Antd)
+
 // router
+import router from './router'
+
 app.use(router)
+
+// 项目初始化
+import './assets/main.css'
+import init from "./init/init.ts";
+
+init()
+
 app.mount('#app')
